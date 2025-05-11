73f85ba56afa50e63d4e72185a4a447e13b03aa5
viettt03 <viettt03@gmail.com>
2025-05-11 15:34:40 +0700

    add cart service

--- server/routes/cart.route.ts
+++ server/routes/cart.route.ts
@@ -1,15 +1,12 @@
-// backend/routes/cart.route.ts
 import express from "express";
 import {
-    addToCart,
-    clearCart,
-    getCart,
-    removeFromCart,
+  addToCart,
+  clearCart,
+  getCart,
+  removeFromCart,
 } from "../controllers/cart.controller";
 import { isAutheticated } from "../middleware/auth";
-
 const cartRouter = express.Router();
-
 cartRouter.post("/add-to-cart", isAutheticated, addToCart);
 cartRouter.post("/remove-from-cart", isAutheticated, removeFromCart);
 cartRouter.get("/get-cart", isAutheticated, getCart);
